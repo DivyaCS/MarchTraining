0a01a21fb23f0c9e9ac86f33e7be7397ccd7c2bd
DVYCSFST <DVYCSFST@Fastest.local>
2025-03-12 20:05:08 -0700

    test

--- src/main/java/Test/SampleDemo.java
+++ src/main/java/Test/SampleDemo.java
@@ -1,6 +1,10 @@
+package Test;
+import org.testng.annotations.Test;
 
 public class SampleDemo {
 
+	@Test
+	
 	public void test() {
 		System.out.println("Training Demo");
 	}
---
+++ src/test/java/SampleDemoTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class SampleDemoTest {
+
+  @Test
+  public void testTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
